ecdf02097704d51e022da4a221ec851f3337d90b
knightking100 <herokingcol@gmail.com>
2018-08-30 13:45:57 +0700

    Update dependencies

--- update_deps.ps1
+++ update_deps.ps1
@@ -1,3 +1,3 @@
-git rm -rf System.Text.JsonLab
+git rm -rf .\System.Text.JsonLab
 git read-tree --prefix=System.Text.JsonLab/ -u gitcorefxlab/master:src/System.Text.JsonLab
 git commit -a -m "Update dependencies"
